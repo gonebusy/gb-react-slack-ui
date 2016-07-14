b0485369d69a2221adcc4d25f5f88650bf03feb9
Richard Lehmann <richard@Richards-MacBook-Pro.local>
2016-07-14 09:39:19 -0400

    added what you can do sections. Updated copy to make sense in the "GoneBusy is for" sections. Added prismjs for syntax coloring (need to cleanup ruby snippet in main.js )

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,31 @@ var YTManager = require('./components/yt-manager');
 var Bootstrap = require('bootstrap');
 var Validator = require("bootstrap-validator")
 var Waypoints = require('waypoints');
+var Prism = require('prismjs');
 var CSSPlugin = require('gsap-css-plugin');
 var EasePack = require('gsap-ease-pack');
 var ScrollToPlugin = require('gsap-scroll-to-plugin');
 var SplitText = require('gsap-split-text');
 var Header = require('./components/header');
 
+Prism.languages.ruby = {
+	'comment': /#[^\r\n]*(\r?\n|$)/g,
+	'string': /("|')(\\?.)*?\1/g,
+	'regex': {
+		pattern: /(^|[^/])\/(?!\/)(\[.+?]|\\.|[^/\r\n])+\/[gim]{0,3}(?=\s*($|[\r\n,.;})]))/g,
+		lookbehind: true
+	},
+	'keyword': /\b(alias|and|BEGIN|begin|break|case|class|def|define_method|defined|do|each|else|elsif|END|end|ensure|false|for|if|in|module|new|next|nil|not|or|raise|redo|rescue|retry|return|self|super|then|throw|true|undef|unless|until|when|while|yield)\b/g,
+  'builtin': /\b(Array|Bignum|Binding|Class|Continuation|Dir|Exception|FalseClass|File|Stat|File|Fixnum|Fload|Hash|Integer|IO|MatchData|Method|Module|NilClass|Numeric|Object|Proc|Range|Regexp|String|Struct|TMS|Symbol|ThreadGroup|Thread|Time|TrueClass)\b/,
+	'boolean': /\b(true|false)\b/g,
+	'number': /\b-?(0x)?\d*\.?\d+\b/g,
+	'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g,
+  'inst-var': /[@&]\b[a-zA-Z_][a-zA-Z_0-9]*[?!]?\b/g,
+  'symbol': /:\b[a-zA-Z_][a-zA-Z_0-9]*[?!]?\b/g,
+  'const': /\b[A-Z][a-zA-Z_0-9]*[?!]?\b/g,
+	'ignore': /&(lt|gt|amp);/gi,
+	'punctuation': /[{}[\];(),.:]/g
+};
 
 global.Main = function(){
 
