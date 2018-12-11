2a1e2ea36ee44dc717a8e09ff1ae9778f736e63a
Richard Lehmann <rlehmann@atomicfarmer.com>
2018-12-11 18:07:33 -0500

    oops forgot make it loop again

--- src/js/components/slack.js
+++ src/js/components/slack.js
@@ -49,8 +49,6 @@ export default class Slack extends Component {
           });
         });
 
-        return;
-
         TweenLite.delayedCall(8, () => {
           this.setState({
             showResponse: false
